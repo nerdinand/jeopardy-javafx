859ba697ba9e4529a458e54d642b6676d25211fe
Ferdinand Niedermann <ferdinand.niedermann@simplificator.com>
2014-06-21 23:28:06 +0200

    + ability to edit player scores manually

--- software/src/main/java/com/nerdinand/jeopardy/controllers/MainWindowController.java
+++ software/src/main/java/com/nerdinand/jeopardy/controllers/MainWindowController.java
@@ -9,6 +9,7 @@ import com.nerdinand.jeopardy.models.Frame;
 import com.nerdinand.jeopardy.models.MediaType;
 import com.nerdinand.jeopardy.models.Player;
 import com.nerdinand.jeopardy.models.Players;
+import com.nerdinand.jeopardy.models.Score;
 import com.nerdinand.jeopardy.services.Util;
 import com.nerdinand.jeopardy.view.AnswerWindow;
 import com.nerdinand.jeopardy.view.MainWindow;
@@ -22,6 +23,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
@@ -48,8 +50,10 @@ public class MainWindowController implements Initializable, FrameAnsweredListene
     private Label[] playerLabels;
     private PlayerKeyEventListener playerKeyEventListener;
     private Map<Frame, Button> frameButtons;
+
     @FXML
     private GridPane gridPane;
+
     private AudioClip answerSound;
 
     @Override
@@ -92,7 +96,7 @@ public class MainWindowController implements Initializable, FrameAnsweredListene
 
     private void openAnswerWindow(Frame frame) {
         AnswerWindow answerWindow = new AnswerWindow(frame, this);
-        
+
         Button buttonForFrame = getButtonForFrame(frame);
         buttonForFrame.setDisable(true);
 
@@ -104,7 +108,7 @@ public class MainWindowController implements Initializable, FrameAnsweredListene
             stage.setTitle(frame.toString());
             stage.setScene(scene);
             stage.show();
-            
+
             if (frame.getAnswer().getMediaType() == MediaType.SOUND) {
                 final String uri = frame.getAnswer().getMediaPath().toURI().toString();
                 this.answerSound = new AudioClip(uri);
@@ -174,17 +178,55 @@ public class MainWindowController implements Initializable, FrameAnsweredListene
                 .owner(null)
                 .title("Game over!")
                 .masthead(null)
-                .message("The game is over and the winner is "+Players.getWinner()+"!")
+                .message("The game is over and the winner is " + Players.getWinner() + "!")
                 .showInformation();
     }
 
     private EventHandler<WindowEvent> createOnCloseHandler() {
         return (WindowEvent event) -> {
-            if (answerSound != null) {
-                answerSound.stop();
-            }
-            
-            Assets.jeopardyMusic.stop();
+            stopSounds();
         };
+    }
+
+    private void stopSounds() {
+        if (answerSound != null) {
+            answerSound.stop();
+        }
+
+        Assets.jeopardyMusic.stop();
+    }
+
+    @FXML
+    private void onPlayerLabel1Click(MouseEvent e) {
+        editPlayerScore(Players.getPlayerList().get(0));
+    }
+
+    @FXML
+    private void onPlayerLabel2Click(MouseEvent e) {
+        editPlayerScore(Players.getPlayerList().get(1));
+    }
+
+    @FXML
+    private void onPlayerLabel3Click(MouseEvent e) {
+        editPlayerScore(Players.getPlayerList().get(2));
+    }
+
+    @FXML
+    private void onPlayerLabel4Click(MouseEvent e) {
+        editPlayerScore(Players.getPlayerList().get(3));
+    }
+
+    private void editPlayerScore(Player player) {
+        final int oldScore = player.getPoints();
+        String newScore = Dialogs.create()
+                .owner(null)
+                .title(player.getName())
+                .masthead(null)
+                .message("Edit " + player.getName() + "'s score:")
+                .showTextInput("" + oldScore);
+
+        player.addScore(new Score(player, null, Integer.parseInt(newScore) - oldScore));
+
+        updatePlayerStatuses();
     }
 }
